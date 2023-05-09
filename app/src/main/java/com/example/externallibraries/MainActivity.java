26e4ea31d7e5305f20f145b3dbbfc6dc6434b411
d22samma <d22samma@student.his.se>
2023-05-09 11:37:38 +0200

    Add ImageView from url inside MainActivity.java

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.externallibraries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        String url = "https://wallpaperaccess.com/full/1971371.jpg";
+        Glide.with(MainActivity.this)
+                .load(url)
+                .into(imageView);
     }
 }
